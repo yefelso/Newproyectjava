56029340ed89affb036f0a3b0dd8ccbd510391a6
yefelso <1313124@senati.pe>
2023-03-10 22:21:18 -0500

    Myproyectjava

---
+++ Mysecondproyect/src/mysecondproyect/Mysecondproyect.java
@@ -0,0 +1,17 @@
+package mysecondproyect;
+
+/**
+ *
+ * @author USER
+ */
+public class Mysecondproyect {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello again world");
+    }
+    
+}
